a06fff1c210842c01dcd8dbeb730b99c076d0c71
Jocelyn Wang <jocelynw0527@gmail.com>
2022-08-15 12:17:39 -0400

    Updated to solve problem Category 1 (Issue-#10)

--- Firefox-analysis-mode-crawler/crawler.js
+++ Firefox-analysis-mode-crawler/crawler.js
@@ -29,13 +29,45 @@ fs.createReadStream("sites.csv")
         const page = await browser.newPage();
 
         try {
-            await page.goto(sites[site], { waitUntil: "domcontentloaded" });
+          await page.goto(sites[site], { waitUntil: "domcontentloaded" });
             console.log("Navigated!");
         } catch {
-            console.log("Site loading went wrong")
+            console.log("Site loading went wrong");
         } 
+      
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        // Reloading removes popup ad
+        try {
+          await page.reload({ waitUntil: "domcontentloaded" });
+          console.log("Reloaded");
+        } catch {
+          console.log("Not reloaded");
+        } 
+
+        await new Promise(resolve => setTimeout(resolve, 5000));
         
-        await new Promise(resolve => setTimeout(resolve, 10000));
+        /* One site has popup ad only when you load it the second time,
+          reload again to remove ad */
+        try {
+          await page.reload({ waitUntil: "domcontentloaded" });
+          console.log("Reloaded");
+        } catch {
+          console.log("Not reloaded");
+        } 
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        /* Some sites put the crawler in a text box. 
+          Clicking on a non-hyperlink element solves the problem. */
+        try {
+          await page.waitForSelector('h1');
+          await page.click('h1');
+          console.log('Clicked')
+        } catch {
+          console.log("Not clicked")
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 5000));
 
         /* Promise.race sets time limit for page.keyboard.down to resolve the issue
            that page.keyboard.down is never rejected or resolved on some sites */
@@ -43,9 +75,9 @@ fs.createReadStream("sites.csv")
         await Promise.race([page.keyboard.down('Shift'), new Promise(resolve => setTimeout(resolve, 5000))]);
         await Promise.race([page.keyboard.down('KeyA'), new Promise(resolve => setTimeout(resolve, 5000))]);
   
-        // Allow the site to reload after analysis is triggered
+        // Allow the site to load after analysis is triggered
         await new Promise(resolve => setTimeout(resolve, 30000));
-        
+
         await Promise.race([page.keyboard.down('Shift'), new Promise(resolve => setTimeout(resolve, 5000))]);
         await Promise.race([page.keyboard.down('Alt'), new Promise(resolve => setTimeout(resolve, 5000))]);
         await Promise.race([page.keyboard.down('KeyS'), new Promise(resolve => setTimeout(resolve, 5000))]);
@@ -57,3 +89,4 @@ fs.createReadStream("sites.csv")
     }
     console.log("---------------- ALL TESTING DONE ----------------")
 })();
+
