6e4801e31b23369ebffe162da21cab5aae439734
Jocelyn Wang <jocelynw0527@gmail.com>
2022-08-16 13:35:47 -0400

    Revert "Fixed text box issue (Issue-#10)"
    This reverts commit 109bbb578dc416234c3b5ba7e8b75b9282b89e76.

--- Firefox-analysis-mode-crawler/crawler.js
+++ Firefox-analysis-mode-crawler/crawler.js
@@ -20,56 +20,71 @@ fs.createReadStream("sites.csv")
 
 (async () => {
     const browser = await puppeteer.launch(firefoxOptions);
-    const page = await browser.newPage();
+
     // Allow time to load the extension
-    await new Promise(resolve => setTimeout(resolve, 30000));
+    await new Promise(resolve => setTimeout(resolve, 60000));
 
     for (let site in sites) {
         console.log(sites[site]);
+        const page = await browser.newPage();
 
         try {
           await page.goto(sites[site], { waitUntil: "domcontentloaded" });
-          console.log("Navigated!");
+            console.log("Navigated!");
         } catch {
-          console.log("Site loading went wrong");
+            console.log("Site loading went wrong");
         } 
-        
-        await new Promise(resolve => setTimeout(resolve, 10000));
+      
+        await new Promise(resolve => setTimeout(resolve, 5000));
 
-        // Some sites put the crawler in a text box. Clicking on a non-hyperlink element solves the problem. 
+        // Reloading removes popup ad
         try {
-          await Promise.race([page.keyboard.down('Shift'), new Promise(resolve => setTimeout(resolve, 5000))]);
-          await Promise.race([page.keyboard.down('Tab'), new Promise(resolve => setTimeout(resolve, 5000))]);
+          await page.reload({ waitUntil: "domcontentloaded" });
+          console.log("Reloaded");
+        } catch {
+          console.log("Not reloaded");
+        } 
+
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        
+        /* One site has popup ad only when you load it the second time,
+          reload again to remove ad */
+        try {
+          await page.reload({ waitUntil: "domcontentloaded" });
+          console.log("Reloaded");
+        } catch {
+          console.log("Not reloaded");
+        } 
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        /* Some sites put the crawler in a text box. 
+          Clicking on a non-hyperlink element solves the problem. */
+        try {
+          await page.waitForSelector('h1');
+          await page.click('h1');
           console.log('Clicked')
         } catch {
           console.log("Not clicked")
         }
 
-        await new Promise(resolve => setTimeout(resolve, 10000));
+        await new Promise(resolve => setTimeout(resolve, 5000));
 
-        // Promise.race sets time limit for page.keyboard.down to resolve the issue that page.keyboard.down is never rejected or resolved on some sites
+        /* Promise.race sets time limit for page.keyboard.down to resolve the issue
+           that page.keyboard.down is never rejected or resolved on some sites */
         await Promise.race([page.keyboard.down('Alt'), new Promise(resolve => setTimeout(resolve, 5000))]);
         await Promise.race([page.keyboard.down('Shift'), new Promise(resolve => setTimeout(resolve, 5000))]);
         await Promise.race([page.keyboard.down('KeyA'), new Promise(resolve => setTimeout(resolve, 5000))]);
   
         // Allow the site to load after analysis is triggered
-        await new Promise(resolve => setTimeout(resolve, 10000));
+        await new Promise(resolve => setTimeout(resolve, 30000));
 
-        try {
-          await Promise.race([page.keyboard.down('Shift'), new Promise(resolve => setTimeout(resolve, 5000))]);
-          await Promise.race([page.keyboard.down('Tab'), new Promise(resolve => setTimeout(resolve, 5000))]);
-          console.log('Clicked')
-        } catch {
-          console.log("Not clicked")
-        }
-
-        await new Promise(resolve => setTimeout(resolve, 10000));
         await Promise.race([page.keyboard.down('Shift'), new Promise(resolve => setTimeout(resolve, 5000))]);
         await Promise.race([page.keyboard.down('Alt'), new Promise(resolve => setTimeout(resolve, 5000))]);
         await Promise.race([page.keyboard.down('KeyS'), new Promise(resolve => setTimeout(resolve, 5000))]);
         
-        await new Promise(resolve => setTimeout(resolve, 10000));
-      
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        
+        await page.close();
         console.log("testing done");
     }
     console.log("---------------- ALL TESTING DONE ----------------")
