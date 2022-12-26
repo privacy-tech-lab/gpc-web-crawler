1a4aa85dc416a2202e4496a83fc8203016996a4d
Kate Hausladen <40359590+katehausladen@users.noreply.github.com>
2022-12-26 16:58:48 -0500

    adding preliminary post implementation
    I have a successful implementation of POST for the database used in the tutorial Jocelyn and I followed. We will need to change variable/database names so it matches Jocelyn's database setup. This syntax worked when I tested it with Postman using http://localhost:8080/warehouses/ as the link.
    
    The other edit I made was removing connection.end() in line 18. This is because when this line was there, I was only able to send 1 request at a time and then the app would need to be restarted.
    
    All the changes in quotes/spacing were auto changes by Prettier.

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,28 +1,46 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const connection = require('./databse.js');
+const bodyParser = require("body-parser");
+const connection = require("./databse.js");
 
-app.get('/', (req,res) => res.send('Try: /analysis') );
+app.get("/", (req, res) => res.send("Try: /analysis"));
 
-app.get('/status', (req, res) => res.send('Success.') );
+app.get("/status", (req, res) => res.send("Success."));
 
-app.get('/analysis', (req, res) => {
+app.get("/analysis", (req, res) => {
   connection.query(
     "SELECT * FROM analysis.entries",
     (error, results, fields) => {
-      if(error) throw error;
+      if (error) throw error;
       res.json(results);
-      connection.end();
     }
   );
 });
 
+// Preliminary implementation of post. This is for the
+// database described in the tutorial here:
+// https://billmartin.io/blog/how-to-build-and-deploy-a-nodejs-api-on-google-cloud
+// TODO: change variable/database names so that
+// they match Jocelyn's database setup
+
+// app.post("/warehouses/save", (req, res) => {
+//   var identification = req.query.id;
+//   var name = req.query.name;
+//   var zip = req.query.zip;
+//   connection.query(
+//     "INSERT INTO `acme`.`warehouses`() VALUES (?,?,?)",
+//     [identification, name, zip],
+//     (error, results, fields) => {
+//       if (error) throw error;
+//       res.json(results);
+//     }
+//   );
+// });
+
 // Use port 8080 by default, unless configured differently in Google Cloud
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-   console.log(`App is running at: http://localhost:${port}`);
+  console.log(`App is running at: http://localhost:${port}`);
 });
-
