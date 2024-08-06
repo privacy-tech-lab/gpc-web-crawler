4791b8967d9415821f5e24b8949ba4616b644d0d
franciscawijaya <155489035+franciscawijaya@users.noreply.github.com>
2024-08-06 23:59:42 +0700

    Tried using a different version of selenium

--- selenium-optmeowt-crawler/local-crawler.js
+++ selenium-optmeowt-crawler/local-crawler.js
@@ -9,7 +9,7 @@ var total_begin = Date.now(); //start logging time
 var err_obj = new Object();
 // Loads sites to crawl
 const sites = [];
-fs.createReadStream("sites1.csv")
+fs.createReadStream("sites.csv")
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
     sites.push(row[0]);
