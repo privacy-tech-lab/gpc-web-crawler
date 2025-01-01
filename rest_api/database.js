0400c5d4893980cd267ef5c7a01817c1fce66716
Ebuka Akubilo <eakubilo@wesleyan.edu>
2025-01-01 02:45:42 -0500

    shrunk code diff by reverting package-lock and use connection pool instead of single connection thread in database

--- rest_api/database.js
+++ rest_api/database.js
@@ -1,27 +1,26 @@
 const mysql = require("mysql");
 
-var config = {
+const config = {
   user: process.env.DB_USERNAME,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: 3306,
+  connectionLimit: 10
 };
 
-// When running from localhost, get the config from .env
-// else {
-console.log("Running from localhost. Connecting to DB directly.");
-config.host = process.env.DB_HOST;
-config.port = 3306;
-// }
 
-let connection = mysql.createConnection(config);
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("Error connecting: " + err.stack);
-    return;
-  }
-  console.log("Connected as thread id: " + connection.threadId);
+const pool = mysql.createPool(config);
+
+// Optional: log when connections are created
+pool.on('connection', function (connection) {
+  console.log('DB Connection established');
 });
 
+// Optional: log errors
+pool.on('error', function (err) {
+  console.error('DB Pool error:', err);
+});
 
-module.exports = connection;
+module.exports = pool;
