6f34e8bdaa339c622524b8448850acc3ac994e07
Ebuka Akubilo <eakubilo@wesleyan.edu>
2025-02-24 11:40:46 -0500

    removed more GPC stuff from crawl code

--- selenium-optmeowt-crawler/DatabaseManager.js
+++ selenium-optmeowt-crawler/DatabaseManager.js
@@ -15,35 +15,6 @@ class DatabaseManager {
   }
 
   /**
-   * Logs an error encountered during crawling or GPC endpoint checking to a JSON file.
-   * @async
-   * @param {string} site - The URL of the site where the error occurred.
-   * @param {string} error - The error message.
-   */
-  async logError(site, error) {
-    const errors = await this.loadErrors();
-    errors[site] = error;
-    await fs.promises.writeFile(
-      `${this.crawl_path}/well-known-errors.json`,
-      JSON.stringify(errors, null, 2)
-    );
-  }
-
-  /**
-   * Loads previously logged errors from the JSON file.
-   * @async
-   * @returns {Promise<object>} An object containing the logged errors, keyed by site URL.
-   */
-  async loadErrors() {
-    try {
-      const data = await fs.promises.readFile(`${this.crawl_path}/well-known-errors.json`, 'utf8');
-      return JSON.parse(data);
-    } catch {
-      return {};
-    }
-  }
-
-  /**
    * Checks the database for existing analysis records for a given site and updates the site ID if necessary.
    * Prioritizes updating existing entries where the site_id is null. If no such entry exists, it tries to update a null analysis entry.
    * @async
@@ -51,12 +22,10 @@ class DatabaseManager {
    * @param {number} siteId - The ID of the site to be updated in the database.
    * @returns {Promise<boolean>} True if the database was updated, false otherwise.
    */
-  async checkAndUpdateDB(site, siteId) {
+  async checkAndUpdateDB(siteId) {
     try {
       const response = await axios.get(`${API_BASE_URL}/analysis/${siteId}`);
       const latestData = response.data;
-      console.log(`tried to check if ${siteId} was in database, result: ${latestData} `)
-      console.log(latestData.length)
       if (latestData.length >= 1) {
         return true;
       } 
